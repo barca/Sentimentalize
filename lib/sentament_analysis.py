678654d2d97a4909fc2e1d513f2f3ef1a59b7190
barca <tharden@wesleyan.edu>
2014-09-13 18:13:25 -0400

    finalized sentament_analysis, view history in newspin

--- lib/sentament_analysis.py
+++ lib/sentament_analysis.py
@@ -2,11 +2,13 @@ import re
 import json
 json_data = open('words.json').read()
 lib = json.loads(json_data)
-class scored_words:
-  def __init___(self):
-    self.score
-    self.comparitave
-    self.words
+
+class scored_words():
+  def __init__(self,score,comparitave,words = []):
+    self.score = score
+    self.comparitave = comparitave
+    self.words = words
+
 def change_score(tally,phrase,is_neg):
     no_punctuation = re.sub('/[^a-zA-Z]+/g', " ",phrase)
     no_punctuation = re.sub('/ {2, }/', ' ',no_punctuation)
@@ -14,24 +16,29 @@ def change_score(tally,phrase,is_neg):
     leng = len(tokens)
     for each in tokens:
       if(is_neg):
-          if lib[each] and lib[each] < 0:
-            tally = add_append(each,lib[each],tally,leng)
+          if lib.get(each) and lib.get(each) < 0:
+            tally = add_append(each,lib.get(each),tally,leng)
       else:
-          if lib[each] and lib[each] > 0:
-            tally = add_append(each,lib[each],tally,leng)
+          if lib.get(each) and lib.get(each) > 0:
+            tally = add_append(each,lib.get(each),tally,leng)
 
     return tally
-def add_append(t,score,tally,l):
-  tally.score = score - hits
-  tally.comparitave = score/l
-  tally.words = words.append(t)
+
+def add_append(t,hits,tally,l):
+  tally.score = tally.score - hits
+  tally.comparitave = tally.score/l
+  if tally.words:
+    tally.words.append(t)
+  else:
+    tally.words = [t]
   return tally
 
 def analyze_get_score(phrase,need_score):
-  tally = scored_word(0,0,[])
+  tally = scored_words(0,0,[])
   tally = change_score(tally,phrase,True)
-  tally = change_score(tally,phase,False)
+  tally = change_score(tally,phrase,False)
   if(need_score):
     return tally.score
   else:
     return tally.comparitave
+
